b97ecd49424508d74660b67df8cd0d753eba7974
ivan921345 <ivanstepanenko921@gmail.com>
2024-03-15 13:52:56 +0100

    added files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const MainSliuder = new Swiper('.weeklyTopSection__sliderWrapper',{
+	navigation:{
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	// allowTouchMove: false,
+	centeredSlides:true,
+	freeMode: true,
+	grabCursor: true,
+	slidesPerView:1,
+
+});
